11b5a56703a7daa21fad9f70771b31b4cd47061b
vyonna <vyonna.njenga@student.moringaschool.com>
2021-12-29 19:32:40 +0300

    added hover function to portfolio section

--- script.js
+++ script.js
@@ -25,3 +25,60 @@
   $(".image3").toggle();
 });
 });
+$(".one").hover(function() {
+  $(".p1").fadeTo("slow",1);
+  $(".img1").fadeTo("slow",0.4);
+  },function() {
+    $(".p1").fadeTo("slow",0);
+    $(".img1").fadeTo("slow",1);
+  });
+$(".two").hover(function() {
+  $(".p2").fadeTo("slow",1);
+  $(".img2").fadeTo("slow",0.4);
+  },function() {
+    $(".p2").fadeTo("slow",0);
+    $(".img2").fadeTo("slow",1);
+  });
+$(".three").hover(function() {
+  $(".p3").fadeTo("slow",1);
+  $(".img3").fadeTo("slow",0.4);;
+  },function() {
+    $(".p3").fadeTo("slow",0);
+    $(".img3").fadeTo("slow",1);
+  });
+$(".four").hover(function() {
+  $(".p4").fadeTo("slow",1);
+  $(".img4").fadeTo("slow",0.4);
+  },function() {
+    $(".p4").fadeTo("slow",0);
+    $(".img4").fadeTo("slow",1);
+  });
+$(".five").hover(function() {
+  $(".p5").fadeTo("slow",1);
+  $(".img5").fadeTo("slow",0.4);
+  },function() {
+    $(".p5").fadeTo("slow",0);
+    $(".img5").fadeTo("slow",1);
+  });
+$(".six").hover(function() {
+  $(".p6").fadeTo("slow",1);
+  $(".img6").fadeTo("slow",0.4);
+  },function() {
+    $(".p6").fadeTo("slow",0);
+    $(".img6").fadeTo("slow",1);
+  });
+$(".seven").hover(function() {
+  $(".p7").fadeTo("slow",1);
+  $(".img7").fadeTo("slow",0.4);
+  },function() {
+    $(".p7").fadeTo("slow",0);
+    $(".img7").fadeTo("slow",1);
+  });
+$(".eight").hover(function() {
+  $(".p8").fadeTo("slow",1);
+  $(".img8").fadeTo("slow",0.4);
+  },function() {
+    $(".p8").fadeTo("slow",0);
+    $(".img8").fadeTo("slow",1);
+  });
+  
