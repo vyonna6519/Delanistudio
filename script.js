b42a063e460a6b4e5c8dd98486c10dfa360cb199
vyonna <vyonna.njenga@student.moringaschool.com>
2021-12-29 19:09:38 +0300

    added js toggle funcion to paragrapghs in what section

--- script.js
+++ script.js
@@ -4,12 +4,24 @@
     $(".des-p").toggle();
     $(".image1").toggle();
  });
+ $(".des-p").click(function() {
+  $(".des-p").toggle();
+  $(".image1").toggle();
+});
     $(".development").click(function(){
     $(".dev-p").toggle();
     $(".image2").toggle();
  });
+ $(".dev-p").click(function() {
+  $(".dev-p").toggle();
+  $(".image2").toggle();
+});
     $(".product").click(function(){
     $(".prod-p").toggle();
     $(".image3").toggle();
  }); 
+ $(".prod-p").click(function() {
+  $(".prod-p").toggle();
+  $(".image3").toggle();
+});
 });
