4d7fd66c398c4e460c437f3abb2ba0c1f5f2a674
vyonna <vyonna.njenga@student.moringaschool.com>
2022-01-02 10:03:58 +0300

    added hide and show function to what section

--- script.js
+++ script.js
@@ -1,84 +1,29 @@
-
-  $(document).ready(function(){
-    $(".design").click(function(){
-    $(".des-p").toggle();
-    $(".image1").toggle();
- });
- $(".des-p").click(function() {
-  $(".des-p").toggle();
-  $(".image1").toggle();
+$(document).ready(function(){
+    
+  $(".image1").click(function(){ 
+      $("p.par-1").show();
+      $(".image1").hide(); 
+  });
+  $("p.par-1").click(function(){
+      $("p.par-1").hide();
+      $(".image1").show(); 
+  });
+   $(".image2").click(function(){
+       $("p.par-2").show();   
+       $(".image2").hide();
+   });
+   $("p.par-2").click(function(){
+       $("p.par-2").hide();
+       $(".image2").show();
+   });
+   $(".image3").click(function(){
+       $("p.par-3").show();   
+       $(".image3").hide();
+   });
+   $("p.par-3").click(function(){
+       $(".image3").show();
+       $("p.par-3").hide();
+   }); 
 });
-    $(".development").click(function(){
-    $(".dev-p").toggle();
-    $(".image2").toggle();
- });
- $(".dev-p").click(function() {
-  $(".dev-p").toggle();
-  $(".image2").toggle();
-});
-    $(".product").click(function(){
-    $(".prod-p").toggle();
-    $(".image3").toggle();
- }); 
- $(".prod-p").click(function() {
-  $(".prod-p").toggle();
-  $(".image3").toggle();
-});
-});
-$(".one").hover(function() {
-  $(".p1").fadeTo("slow",1);
-  $(".img1").fadeTo("slow",0.4);
-  },function() {
-    $(".p1").fadeTo("slow",0);
-    $(".img1").fadeTo("slow",1);
-  });
-$(".two").hover(function() {
-  $(".p2").fadeTo("slow",1);
-  $(".img2").fadeTo("slow",0.4);
-  },function() {
-    $(".p2").fadeTo("slow",0);
-    $(".img2").fadeTo("slow",1);
-  });
-$(".three").hover(function() {
-  $(".p3").fadeTo("slow",1);
-  $(".img3").fadeTo("slow",0.4);;
-  },function() {
-    $(".p3").fadeTo("slow",0);
-    $(".img3").fadeTo("slow",1);
-  });
-$(".four").hover(function() {
-  $(".p4").fadeTo("slow",1);
-  $(".img4").fadeTo("slow",0.4);
-  },function() {
-    $(".p4").fadeTo("slow",0);
-    $(".img4").fadeTo("slow",1);
-  });
-$(".five").hover(function() {
-  $(".p5").fadeTo("slow",1);
-  $(".img5").fadeTo("slow",0.4);
-  },function() {
-    $(".p5").fadeTo("slow",0);
-    $(".img5").fadeTo("slow",1);
-  });
-$(".six").hover(function() {
-  $(".p6").fadeTo("slow",1);
-  $(".img6").fadeTo("slow",0.4);
-  },function() {
-    $(".p6").fadeTo("slow",0);
-    $(".img6").fadeTo("slow",1);
-  });
-$(".seven").hover(function() {
-  $(".p7").fadeTo("slow",1);
-  $(".img7").fadeTo("slow",0.4);
-  },function() {
-    $(".p7").fadeTo("slow",0);
-    $(".img7").fadeTo("slow",1);
-  });
-$(".eight").hover(function() {
-  $(".p8").fadeTo("slow",1);
-  $(".img8").fadeTo("slow",0.4);
-  },function() {
-    $(".p8").fadeTo("slow",0);
-    $(".img8").fadeTo("slow",1);
-  });
+  
   
